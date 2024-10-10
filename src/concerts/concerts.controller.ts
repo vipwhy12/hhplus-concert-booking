0227ff7b7cef7b721cd678c9d35b3af3eadc9ec0
vipwhy12 <vipwhy12@naver.com>
2024-10-11 05:51:06 +0900

    test: 콘서트 날짜 조회 mockAPI 구현

--- src/concerts/concerts.controller.ts
+++ src/concerts/concerts.controller.ts
@@ -1,7 +1,35 @@
-import { Body, Controller, Param, Post, Headers } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  Headers,
+  Get,
+  Query,
+} from '@nestjs/common';
 
 @Controller('concerts')
 export class ConcertsController {
+  @Get(':concertId/sessions/:sessionId/dates/:date')
+  getAvailableDates(
+    @Param('concertId') concertId: string,
+    @Param('sessionId') sessionId: string,
+    @Param('date') date: string,
+    @Query('status') status: string,
+  ) {
+    const availableDates = ['2024-05-01', '2024-05-02', '2024-05-03'];
+    const constantsName = '흑백요리사';
+    return {
+      statusCode: 200,
+      message: 'success',
+      data: {
+        concertId,
+        constantsName,
+        availableDates,
+      },
+    };
+  }
+
   @Post(':concertId/sessions/:sessionId/payments')
   payForReservedSeat(
     @Param('concertId') concertId: string,
