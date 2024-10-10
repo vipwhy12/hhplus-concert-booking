f665bde813fa9bd87fb2e63bba2dfc900f983c75
vipwhy12 <vipwhy12@naver.com>
2024-10-11 02:41:51 +0900

    chore: 큐 모듈, 컨트롤러 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { QueuesModule } from './queues/queues.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       cache: true,
       // load: [async () => console.log('test')],
     }),
+    QueuesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
