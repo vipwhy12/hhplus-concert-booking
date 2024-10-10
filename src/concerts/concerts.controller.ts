6fb9de94d4d7a8f2e6612922bbc4b94859bd6f8d
vipwhy12 <vipwhy12@naver.com>
2024-10-11 05:27:26 +0900

    test: 콘서트 좌석 결제 MockApi

--- src/concerts/concerts.controller.ts
+++ src/concerts/concerts.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Param, Post, Headers } from '@nestjs/common';
 
 @Controller('concerts')
-export class ConcertsController {}
+export class ConcertsController {
+  @Post(':concertId/sessions/:sessionId/payments')
+  payForReservedSeat(
+    @Param('concertId') concertId: string,
+    @Param('sessionId') sessionId: string,
+    @Body('reservationId') reservationId: string,
+    @Headers('WAITING_TOKEN') waitingToken: string,
+  ) {
+    return {
+      statusCode: 200,
+      message: 'success',
+      data: {
+        reservationId,
+        seatNumber: 25,
+        status: '예약 성공',
+      },
+    };
+  }
+}
