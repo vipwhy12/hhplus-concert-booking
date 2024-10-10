55488ea732eccb05096dec90c0852ee1c2dbde86
vipwhy12 <vipwhy12@naver.com>
2024-10-11 04:34:06 +0900

    test: 포인트 조회 MockApi 구현

--- src/points/points.controller.ts
+++ src/points/points.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('users/:userId/points')
-export class PointsController {}
+export class PointsController {
+  @Get()
+  getPoints(@Param('userId') userId: string) {
+    return {
+      statusCode: '200',
+      message: 'success',
+      data: {
+        userId,
+        point: 5000,
+      },
+    };
+  }
+}
