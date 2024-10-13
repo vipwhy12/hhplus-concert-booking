fd7537352d135a3faa31af9aa29de13e89ea78a2
vipwhy12 <vipwhy12@naver.com>
2024-10-14 00:56:50 +0900

    feat: swagger 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('콘서트 예약 서비스')
+    .setDescription('콘서트 예약 서비스를 위한 API 입니다.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
   const PORT = process.env.PORT || 3000;
 
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 
+  SwaggerModule.setup('api', app, document);
   await app.listen(PORT);
 }
 bootstrap();
