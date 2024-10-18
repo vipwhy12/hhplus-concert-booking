d8f45741cb88d4f510c54f464f2d3c33e8f20d51
vipwhy12 <vipwhy12@naver.com>
2024-10-19 05:07:15 +0900

    refactor: 결제 반환 DTO 적용

--- src/interfaces/api/points/dto/point.dto.ts
+++ src/interfaces/api/points/dto/point.dto.ts
@@ -2,7 +2,7 @@ import { IsNumber } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PointDto {
-  @ApiProperty({ description: '유저 아이디' })
+  @ApiProperty({ description: '포인트' })
   @IsNumber({}, { message: 'amount는 숫자여야 합니다.' })
   amount: number;
 }
