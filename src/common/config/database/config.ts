87a9b0800937c2946ee63944c7eb4a2c70b69581
vipwhy12 <vipwhy12@naver.com>
2024-10-20 23:45:51 +0900

    refactor: TypeOrmOption 분리

---
+++ src/common/config/database/config.ts
@@ -0,0 +1,31 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { ConcertEntity } from 'src/common/entities/concert.entity';
+import { ReservationEntity } from 'src/common/entities/reservation.entity';
+import { SeatEntity } from 'src/common/entities/seat.entity';
+import { SessionEntity } from 'src/common/entities/session.entity';
+import { UserEntity } from 'src/common/entities/user.entity';
+import { PointEntity } from 'src/infrastructure/payments/entity/point.entity';
+import { WaitingQueuesEntity } from 'src/infrastructure/waiting-queues/entity/waiting.queue.entity';
+
+export const typeOrmConfig = (
+  configService: ConfigService,
+): TypeOrmModuleOptions => ({
+  type: 'mysql',
+  host: configService.get<string>('DB_HOST', 'localhost'),
+  port: configService.get<number>('DB_PORT', 3306),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
+  synchronize: true,
+  logging: true,
+  entities: [
+    PointEntity,
+    UserEntity,
+    WaitingQueuesEntity,
+    ConcertEntity,
+    SessionEntity,
+    SeatEntity,
+    ReservationEntity,
+  ],
+});
