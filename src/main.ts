ed1cb95c4f804852197c1a176ad18d3e13e84035
vipwhy12 <vipwhy12@naver.com>
2024-10-25 02:50:02 +0900

    feat: 내장 Logger를 winston Logger로 대체

--- src/main.ts
+++ src/main.ts
@@ -3,17 +3,40 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { apiDocument } from './common/docs/api.document';
+import {
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import * as winston from 'winston';
+import { HttpExceptionFilter } from './common/filter/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const PORT = process.env.PORT || 3000;
   const apiDocumentConfig = new apiDocument().initializeOptions();
   const document = SwaggerModule.createDocument(app, apiDocumentConfig);
-
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  const logger = WinstonModule.createLogger({
+    transports: new winston.transports.Console({
+      level: 'silly',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.ms(),
+        nestWinstonModuleUtilities.format.nestLike('hhplus-concert-booking', {
+          prettyPrint: true,
+          colors: true,
+          appName: true,
+        }),
+      ),
+    }),
+  });
 
   SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useLogger(logger);
+
+  logger.log(`🚀 Server is running on http://localhost:${PORT}`);
+
   await app.listen(PORT);
 }
 
